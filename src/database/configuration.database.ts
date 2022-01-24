df93119f3abd309c49e3b3c41ecabe89dbc3c1e6
Paulo da Luz <paulo.daluzjr@gmail.com>
2022-01-24 13:36:34 -0300

    feat: creating connection with firebase

--- src/database/configuration.database.ts
+++ src/database/configuration.database.ts
@@ -1,22 +1,12 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+import * as admin from 'firebase-admin';
+const { getFirestore } = require('firebase-admin/firestore');
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-};
+import * as serviceAccount from './serviceAccountKey.json';
 
-initializeApp({
-  credential: cert(firebaseConfig),
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as any),
 });
 
 const db = getFirestore();
 
 export { db };
-
-// https://firebase.google.com/docs/firestore/quickstart
