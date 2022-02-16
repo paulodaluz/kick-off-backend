19743432424f1fcb471beed130799866e33fd173
Paulo da Luz <paulo.daluzjr@gmail.com>
2022-02-16 11:09:30 -0300

    feat: unit test user controller

--- test/unit/controller/user.controller.spec.ts
+++ test/unit/controller/user.controller.spec.ts
@@ -19,6 +19,35 @@ describe('UserController test', () => {
     await app.init();
   });
 
+  it('/POST registerUser of a startup with 201 ok', async () => {
+    userService.registerUser = jest.fn().mockResolvedValueOnce({ uuid: MockData.userStartupCreatedResponse.uuid });
+
+    return request(app.getHttpServer())
+      .post(`/user/register`)
+      .send(MockData.userToCreateStartup)
+      .expect(201)
+      .expect((response) => {
+        expect(typeof response.body.uuid).toBe('string');
+        expect(response.body.uuid).toBe(MockData.userStartupCreatedResponse.uuid);
+      });
+  });
+
+  it('/POST loginUser of a startup with 201 ok', async () => {
+    userService.login = jest.fn().mockResolvedValueOnce({ uuid: MockData.userStartupCreatedResponse.uuid });
+
+    return request(app.getHttpServer())
+      .post(`/user/login`)
+      .send({
+        email: 'teste@email.com',
+        password: 'senha123'
+      })
+      .expect(201)
+      .expect((response) => {
+        expect(typeof response.body.uuid).toBe('string');
+        expect(response.body.uuid).toBe(MockData.userStartupCreatedResponse.uuid);
+      });
+  });
+
   it('/GET getUserInfos of a startup with 200 ok', async () => {
     userService.getUserInfos = jest.fn().mockResolvedValueOnce(MockData.userStartupCreatedResponse);
 
@@ -35,5 +64,21 @@ describe('UserController test', () => {
         expect(response.body.phoneNumber).toBe("(54) 99108-3039");
         expect(response.body.cnpj).toBe("98.828.768/0001-52");
       });
+  });
+
+  it('/PUT updateUser 200 ok', async () => {
+    userService.updateUser = jest.fn().mockImplementation();
+
+    return request(app.getHttpServer())
+      .put(`/user/update-info/${MockData.userStartupCreated.uuid}`)
+      .set('Content-type', 'application/json')
+      .send(MockData.userStartupCreated)
+      .expect(200);
+  });
+
+  it('/DELETE deleteUser 200 ok', async () => {
+    userService.deleteUser = jest.fn().mockImplementation();
+
+    return request(app.getHttpServer()).delete(`/user/delete-infos/${MockData.userStartupCreated.uuid}`).expect(200);
   });
 });
