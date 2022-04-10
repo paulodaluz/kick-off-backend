f96f36d3ff32681ddca07a4aa52a50b6ea54581f
Paulo da Luz <paulera.daluz@gmail.com>
2022-04-10 13:25:49 -0300

    fix: conflict

--- src/services/requirement.service.ts
+++ src/services/requirement.service.ts
@@ -187,47 +187,77 @@ export class RequirementService {
       this.requirementRepository.getRequirementByUuid(uuidByRequirement),
     ]);
 
-    const { typeOfUser } = customer;
-    let requirementByClientUpdated;
-
     const reqsWaitingApprovalUpdated = customer.requirementsWaitingApproval.filter(
       (req) => req !== uuidByRequirement,
     );
 
-    if (typeOfUser === 'developer') {
-      requirementByClientUpdated = customer.workInProgress.concat(uuidByRequirement);
-    }
+    const requirementByClientUpdated = this.getRequirementByClientUpdated(
+      interactionStatus,
+      customer,
+      uuidByRequirement,
+    );
 
-    if (typeOfUser === 'investor') {
-      requirementByClientUpdated = customer.investedStartups.concat(uuidByRequirement);
-    }
-
-    Promise.all([
+    const operationsToDo = [
       this.userRepository.updateUserInfo(
         uuidByCustomer,
         this.getDataOfUserToUpdate(
-          typeOfUser,
+          customer.typeOfUser,
           reqsWaitingApprovalUpdated,
           requirementByClientUpdated,
+          interactionStatus,
         ),
       ),
+
       this.notificationService.registerNotification(
         uuidByStartupProprietress,
         uuidByCustomer,
-        `Parabéns seu apoio foi aceito pela Startup ${startupProprietressOfReq.name}
-          , você pode consulta-la em 'Investimentos e contratos recentes'`,
+        this.getMessageToCustomer(interactionStatus, startupProprietressOfReq.name),
         customer.notifications,
       ),
+
       this.notificationService.deleteNotification(
         notificationUuid,
         uuidByStartupProprietress,
         startupProprietressOfReq.notifications,
       ),
-      this.requirementRepository.updateRequirementInfo(
-        uuidByRequirement,
-        this.getDataOfRequirementToUpdate(requeriment, uuidByCustomer),
-      ),
-    ]);
+    ];
+
+    if (interactionStatus === 'accept') {
+      operationsToDo.push(
+        this.requirementRepository.updateRequirementInfo(
+          uuidByRequirement,
+          this.getDataOfRequirementToUpdate(requeriment, uuidByCustomer),
+        ),
+      );
+    }
+
+    await Promise.all(operationsToDo);
+  }
+
+  private getRequirementByClientUpdated(
+    interactionStatus: 'accept' | 'reject',
+    customer: User,
+    uuidByRequirement: string,
+  ): Array<string> | null {
+    if (interactionStatus === 'reject') return null;
+
+    if (customer.typeOfUser === 'investor') {
+      return customer.investedStartups.concat(uuidByRequirement);
+    }
+
+    return customer.workInProgress.concat(uuidByRequirement);
+  }
+
+  private getMessageToCustomer(
+    interactionStatus: 'accept' | 'reject',
+    nameOfStartup: string,
+  ): string {
+    if (interactionStatus === 'accept') {
+      return `Parabéns seu apoio foi aceito pela Startup ${nameOfStartup}
+      , você pode consulta-la em 'Investimentos e contratos recentes'`;
+    }
+
+    return `No momento seu apoio não foi aceito pela Startup ${nameOfStartup}!`;
   }
 
   private getDataOfRequirementToUpdate(
@@ -252,7 +282,12 @@ export class RequirementService {
     typeOfUser: string,
     reqsWaitingApprovalUpdated: Array<string>,
     workInProgressUpdated: Array<string>,
+    interactionStatus: 'accept' | 'reject',
   ): Partial<User> {
+    if (interactionStatus === 'reject') {
+      return { requirementsWaitingApproval: reqsWaitingApprovalUpdated };
+    }
+
     if (typeOfUser === 'investor') {
       return {
         requirementsWaitingApproval: reqsWaitingApprovalUpdated,
