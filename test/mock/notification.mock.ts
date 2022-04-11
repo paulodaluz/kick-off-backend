5d769530ccba4684c56357020f872a6b28c62a23
Paulo da Luz <paulera.daluz@gmail.com>
2022-04-10 22:32:26 -0300

    fix: mock

--- test/mock/notification.mock.ts
+++ test/mock/notification.mock.ts
@@ -14,21 +14,25 @@ export default {
       trigger: '1c9b8694-9581-4c5e-92ce-ac41da534b6f',
       message: 'Sua requisição tem um investidor aguardando análise!',
       id: 'e570f489-f5b2-4827-88f3-9861b345285c',
+      creationDate: 'Sun Apr 10 2022 20:44:52 GMT-0300 (Horário Padrão de Brasília)',
     },
     {
       trigger: '1c9b8694-9581-4c5e-92ce-ac41da534b6f',
       message: 'Sua requisição tem um investidor aguardando análise!',
       id: '5a7c961d-c72a-43f0-9e1a-b3393ffe95a0',
+      creationDate: 'Sun Apr 10 2022 20:44:52 GMT-0300 (Horário Padrão de Brasília)',
     },
     {
       trigger: '1c9b8694-9581-4c5e-92ce-ac41da534b6f',
       id: '47a1992c-56d5-40dc-849b-1596b3b2b7fe',
       message: 'Sua requisição tem um investidor aguardando análise!',
+      creationDate: 'Sun Apr 10 2022 20:44:52 GMT-0300 (Horário Padrão de Brasília)',
     },
     {
       id: '58ece574-e2ea-4439-8570-5cedd56c5555',
       trigger: '1c9b8694-9581-4c5e-92ce-ac41da534b6f',
       message: 'Sua requisição tem um investidor aguardando análise!',
+      creationDate: 'Sun Apr 10 2022 20:44:52 GMT-0300 (Horário Padrão de Brasília)',
     },
   ],
 };
